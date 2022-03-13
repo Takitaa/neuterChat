10bbb7fc2555be226fb37bcd5e5f37b04f40a940
Home Sweet Home <homesweethome@Homes-MacBook-Pro.local>
2022-03-13 21:32:41 +0100

    Vuetify, avatar circle with letter

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,79 @@
 <template>
-	<div id="app">
-		<div class="button" id="button-toggle">
+	<v-app id="app">
+		<div class="button-close" id="button-toggle">
 			<VueToggles
-
 				height="30"
 				width="90"
 				margin="100"
-				checked-text="Chat Off"
-				unchecked-text=" Chat On"
-				:value="buttonToggled"
+				checked-text="Chat On"
+				unchecked-text=" Chat Off"
+				:value="buttonToggle"
 				@click="toggleOn"
 			/>
 		</div>
 
-
-
-			<ToggleButton></ToggleButton>
+		<div class="side-panel">
+			<slide-out dock="right" :visible.sync="visible" title="The title">
+				<div id="chat-container">
+					<p>The content</p>
+					<v-tollbar flat></v-tollbar>
+					<bubble-chat
+						:position="chatPosition"
+						:messages="messagesList"
+						:text-field="'message'"
+						:sender-name-field="'username'"
+						:avatar-link-field="''"
+					>
+					</bubble-chat>
+				</div>
+			</slide-out>
 		</div>
-	</div>
+	</v-app>
 </template>
 
 <script>
-import ToggleButton from "./components/ToggleButton.vue"
-
 export default {
-	name: "App",
-	components: {
-		ToggleButton,
-	},
 	data() {
 		return {
-			buttonToggled: false,
+			buttonToggle: false,
 			visible: false,
-			value: undefined,
+
+			messagesList: [
+				{
+					username: 'John Doe',
+					message: 'Hey guys! How are you?',
+					iconUrl: 'link',
+				},
+			],
+
 			inputContent: '',
+		};
+	},
 
-			//messageList: []
-			//chatPosition: 'right',
-     messagesList: [
-      {
-          username: 'John Doe',
-          message: 'Hey guys! How are you?',
-          iconUrl: 'https://www.link-to-john-doe-pic.jpg'
-      }
-     ]
-    }
-	},  //end Data()
-
-
+	computed: {
+		setImage: function () {
+			return `<img src="https://robohash.org/${this.inputContent}">`;
+		},
+	},
 
 	methods: {
-	// 	toggleOn: function (event) {
-	// 		this.buttonToggled = !this.buttonToggled
-	// 		if (this.buttonToggle = true) {
-	// 			this.visible = true
-	// 		} else {
-	// 			this.visible = false
-	// 		}
-	// },
+		toggleOn: function (toggled) {
+			this.buttonToggle = !this.buttonToggle;
 
+			if (!toggled) {
+				this.visible = true;
+			}
 
+			//console.log(event.target.event);
+			//const statusButtonToggle = event.target.value;
+			//this.$emit("status-toggle", statusButtonToggle);
+		},
 
-
-	//statusPanel = this.visible = !this.visible;
-
-			//return statusPanel
-
-			// if ('this.visible = true') {
-			// 	toggleChat();
-			// }
-
-		}, // end Methods
-
-		// toggleChat: function () {
-		// 	this.isToggled = !this.isToggled;
-		// 	this.isToggled = false;
-		// 	this.visible = false;
-		// },
-
+		handleInputChange: function (event) {
+			this.inputContent = event.target.value;
+		},
+	},
 };
 </script>
 
-<style>
-#app {
-	margin-top: px;
-	font-size: 18px;
-	font-family: 'Roboto', sans-serif;
-	color: blue;
-
-#button {
-		width:45px;
-		float: right;
-		/* pura mágica */
-		position: absolute;
-		top: 50%; /* posiciona na metade da tela */
-		margin-top: -25px;
-	}
-
- #footer {
-            position: fixed;
-            padding: 10px 10px 0px 10px;
-            bottom: 0;
-            width: 100%;
-            /* Height of the footer*/
-            height: 40px;
-            background: grey;
-        }
-}
-</style>
+<style></style>
